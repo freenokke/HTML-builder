76605d24a39fbf91a75fce76690e73aeeb947903
freenokke <kosmatkov94@gmail.com>
2022-05-22 18:32:13 +0300

    fix: 03-files-in-folder fix solution

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,24 +2,18 @@ const fsp = require('fs/promises');
 const fs = require('fs');
 const path = require('path');
 
-function foo(place) {
-  fsp.readdir(place)
-    .then(data => {
-      data.forEach(item => {
-        fs.stat(path.join(place, item), (err, stats) => {
-          if (err) throw err;
-          if (stats.isFile() === false) {
-            foo(path.join(place, item));
-          } else {
-            const itemData = path.parse(item);
-            const itemSize = stats.size; 
-            console.log(`${itemData.name} - ${itemData.ext ? itemData.ext : 'file has no extension'} - ${itemSize} byte`);
-          }
-        });
+fsp.readdir(path.join(__dirname, 'secret-folder'))
+  .then(data => {
+    data.forEach(item => {
+      fs.stat(path.join(__dirname, 'secret-folder', item), (err, stats) => {
+        if (err) throw err;
+        if (stats.isFile() === true) {
+          const itemData = path.parse(item);
+          const itemSize = stats.size; 
+          console.log(`${itemData.name} - ${itemData.ext ? itemData.ext : 'file has no extension'} - ${itemSize} byte`);
+        }
       });
-    }).catch(err => {
-      console.log(err);
     });
-}
-
-foo(__dirname);
+  }).catch(err => {
+    console.log(err);
+  });
